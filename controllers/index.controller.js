abf2396b152e33a3b934c29b971390568ed607d1
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2016-03-02 00:22:03 +0530

    init

---
+++ controllers/index.controller.js
@@ -0,0 +1,4 @@
+exports.user = function (){
+	console.log("controller index");
+	return "user-return";
+}
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var index = require('/../contollers/index.controller.js');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+  console.log(index.user);
+});
+
+module.exports = router;
