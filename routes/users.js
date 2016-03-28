65cce1c893d8e8fe743aeb3aa9c584a9a9e7f5e2
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2016-03-28 18:00:01 +0530

    new

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/:word', function(req, res){
+	res.send(req.params.word);
+})
 module.exports = router;
