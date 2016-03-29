d36b3a8732493082004fe20ffd4794c60ad89294
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2016-03-29 20:04:29 +0530

    user_register

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var user = require('../controller/userController');
 //console.log(device);
+console.log(user);
 router.post('/register', user.register);
 router.get('/view', user.get_user);
 
