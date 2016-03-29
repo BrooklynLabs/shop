049e9fcbd685c3fcd108778446cb59956db579da
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2016-03-29 17:12:53 +0530

    connection-mongo

--- controller/smsParsingController/controller.js
+++ controller/smsParsingController/controller.js
@@ -2,13 +2,21 @@
 var fs			= require('fs');
 var randomstring= require('randomstring');
 var MongoClient = require('mongodb').MongoClient;
-var url 		= 'mongodb://127.0.0.1:27017/shopylytics';
+var connection_string = '127.0.0.1:27017/shopylytics';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+var url 		= connection_string;
 MongoClient.connect(url, function(err, database){
 	db = database;
 	console.log(db, err);
 })
-// var a = new device.constructor();
-// console.log(a),
+
 module.exports = {
 	store:function (req, res){
 		var obj = req.body;
