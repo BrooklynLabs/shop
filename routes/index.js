8addd0f5e8f8235d033037e6a2786423c1cdeec6
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2016-03-29 16:20:57 +0530

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 var express = require('express');
-var index = require('../controller/index.controller.js');
 var router = express.Router();
+
+var user    = require('./users.js');
+var sms    = require('./smsParsing.routes.js');
+//var analytics = require('./analytics.route.js');
+
+router.use('/api/v1/user', user);
+router.use('/api/v1/sms', sms);
+//router.use('/api/v1/analytics', analytics);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-  console.log(index.user);
 });
 
 module.exports = router;
--- routes/smsParsing.routes.js
+++ routes/smsParsing.routes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var sms = require('../controller/smsParsingController');
 //console.log(device);
-router.post('/sms/parse', sms.store);
+router.post('/parse', sms.store);
 
 
 module.exports = router;
