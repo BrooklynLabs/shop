69726271c40c8f248da71f9161e1c706e8918af3
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2016-03-29 19:30:47 +0530

    user_register

---
+++ controller/userController/index.js
@@ -0,0 +1,6 @@
+var controller = require('./controller.js');
+
+module.exports = {
+	register 	: controller.register,
+	get_user 	: controller.get_user
+}
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var user = require('../controller/userController');
+//console.log(device);
+router.post('/register', user.register);
+router.get('/view', user.get_user);
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-router.get('/:word/:neword', function(req, res){
-	res.send({word: req.params.word, newword:req.params.neword});
-})
 module.exports = router;
