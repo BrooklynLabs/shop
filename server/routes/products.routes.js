7c5807917b5d3a3f641b3e46e5d845b09c13f7c1
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2016-05-08 01:01:25 +0530

    multer-removed

--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var product = require('../controller/productController');
 //console.log(device);
-var multer = require('multer');
-var upload = multer({ dest: '../../public/uploads/' });
+// var multer = require('multer');
+// var upload = multer({ dest: '../../public/uploads/' });
 
-router.post('/', upload.single('images'), product.newProduct);
-router.post('/edit', upload.single('images'), product.editProduct);
+router.post('/',  product.newProduct);
+router.post('/edit', product.editProduct);
 router.get('/', product.listProduct);
 
 
